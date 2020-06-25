c821b10754394b5a212938565c9c148b703bd434
MrWillCom <mr.will.com@outlook.com>
2020-06-25 18:27:53 +0800

    Update home to React.Component pages\index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,46 @@
 import Head from 'next/head'
 import TitleBar from "../components/titleBar"
 import MasterDetail from "../components/masterDetail"
+import Flyout from '../components/flyout'
 
 var siteTitle = "Transmister"
 var windowTitle = "Transmister"
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <TitleBar title={windowTitle}></TitleBar>
-      <MasterDetail master={[
-        {
-          title: "Transmister",
-          description: "Welcome to Transmister."
-        },
-        {
-          title: "B",
-          description: "hello!"
-        },
-        {
-          title: "C",
-          description: "hello!"
-        },
-      ]} detail={<div>hello</div>}></MasterDetail>
-    </div>
-  )
+export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <TitleBar title={windowTitle}></TitleBar>
+        <MasterDetail
+          master={[
+            {
+              title: "Transmister",
+              description: "Welcome to Transmister."
+            },
+            {
+              title: "B",
+              description: "hello!"
+            },
+            {
+              title: "C",
+              description: "hello!"
+            },
+          ]}
+          detail={<div>detail</div>} />
+        <Flyout>
+          <h2>helo</h2>
+        </Flyout>
+      </div>
+    )
+  }
 }
+
