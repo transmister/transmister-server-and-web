f129f35cf5edaf4426b08914f67a635711f9f129
MrWillCom <mr.will.com@outlook.com>
2020-07-09 15:44:51 +0800

    Update components/flyout.js

--- components/flyout.js
+++ components/flyout.js
@@ -1,37 +1,17 @@
 import styles from './flyout.module.css'
 
-export default class Flyout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-            data: this.props.data
-        };
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date(),
-            data: this.props.data
-        });
-    }
-
-    render() {
-        return (
-            <div className={styles.flyout}>
-                {this.props.children}
-            </div>
-        );
-    }
+function Flyout({ open, children }) {
+    return (
+        open ? <div className={styles.flyout}>{children}</div> : null
+    )
 }
+
+function FlyoutTitle({ leading, title, showCloseButton, closeFlyout }) {
+    return <div className={styles.title}>
+        {leading ? <p className={styles.titleLeading}>{leading}</p> : null}
+        {title ? <p className={styles.titleTitle}>{title}</p> : null}
+        {showCloseButton ? <button className={styles.titleCloseBtn} onClick={() => {closeFlyout(false)}}>&times;</button> : null}
+    </div>
+}
+
+export { Flyout, FlyoutTitle }
