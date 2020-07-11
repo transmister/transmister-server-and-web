5ba0a2cce21ee3ae62072f2dfb621ef38e5b26e2
MrWillCom <mr.will.com@outlook.com>
2020-07-11 14:19:36 +0800

    Add components/titleBar/avatar

--- components/titleBar.js
+++ components/titleBar.js
@@ -1,12 +1,16 @@
 import styles from "./titleBar.module.css"
 import SearchBar from "./titleBar/searchBar"
 import Title from "./titleBar/title"
+import Avatar from "./titleBar/avatar"
 
-export default function TitleBar(props) {
+export default function TitleBar({ title, setFlyoutContent, setFlyoutOpen }) {
+    const [signedIn, setSignedIn] = React.useState(false)
+
     return (
         <div className={styles.titleBar}>
             <SearchBar></SearchBar>
-            <Title text={props.title}></Title>
+            <Title text={title}></Title>
+            <Avatar signedIn={signedIn} setFlyoutContent={setFlyoutContent} setFlyoutOpen={setFlyoutOpen}>Sign In</Avatar>
         </div>
     )
 }
---
+++ components/titleBar/avatar.js
@@ -0,0 +1,26 @@
+import styles from './avatar.module.css'
+import Button from '../button'
+import SignInOrSignUp from '../signInOrSignUp'
+import { FlyoutTitle, FlyoutContent, FlyoutFooter } from '../flyout'
+import { globalVariables } from '../../global/global'
+
+function Avatar({ signedIn, setFlyoutContent, setFlyoutOpen }) {
+    return (signedIn ? <></> :
+        <Button className={`${styles.avatar} ${styles.avatarSignedInFalse}`} onClick={() => {
+            setFlyoutContent(<>
+                <FlyoutTitle title={`Sign in or sign up to ${globalVariables.prodName}`} showCloseButton={true} closeFlyout={setFlyoutOpen} />
+                <FlyoutContent>
+                    <SignInOrSignUp />
+                </FlyoutContent>
+                <FlyoutFooter align='right'>
+                    <Button onClick={() => {
+                        setFlyoutOpen(false)
+                    }}>Cancel</Button>
+                </FlyoutFooter>
+            </>)
+            setFlyoutOpen(true)
+        }}>Sign In</Button>
+    )
+}
+
+export default Avatar
