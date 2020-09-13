b96f5da275e3a2913b5e26b5c7132d27902d3f00
MrWillCom <mr.will.com@outlook.com>
2020-09-13 17:11:17 +0800

    Update ChatView

--- components/chatView.tsx
+++ components/chatView.tsx
@@ -3,7 +3,21 @@ import styles from './chatView.module.css'
 export default function ChatView(props: {
     username?: string
 }) {
-    return (
-        <div>ChatView: {props.username}</div>
-    )
+    return (<div className={styles.chatView}>
+        <div className={styles.chatViewTitleBar}>
+            <h1 className={styles.chatViewTitleBarTitle}>{props.username}</h1>
+        </div>
+        <div className={styles.chatViewMsgs}>
+            {(() => {
+                var items = []
+                for (let i = 0; i < 64; i++) {
+                    items.push(<p><p>hello, {i}</p></p>)
+                }
+                return items
+            })()}
+        </div>
+        <div className={styles.chatViewInputBar}>
+            <textarea className={styles.chatViewInputBarTextArea}></textarea>
+        </div>
+    </div>)
 }
--- components/masterDetail/master.js
+++ components/masterDetail/master.js
@@ -2,14 +2,14 @@ import styles from "./master.module.css"
 
 export default function Master(props) {
     var items = []
-    for (const i in props.data) {
+    props.data.forEach((value) => {
         items.push(
-            <div className={styles.listItem} key={props.data[i].key}>
-                <p className={styles.listItemTitle}>{props.data[i].title}</p>
-                <p className={styles.listItemDescription}>{props.data[i].description}</p>
+            <div className={styles.listItem} key={value.key}>
+                <p className={styles.listItemTitle}>{value.title}</p>
+                <p className={styles.listItemDescription}>{value.description}</p>
             </div>
         )
-    }
+    })
 
     return (
         <div className={styles.master}>
