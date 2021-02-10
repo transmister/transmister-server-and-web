8880d3d48c816128cd73994d6d56a6faee8b6871
MrWillCom <mr.will.com@outlook.com>
2021-02-10 04:25:07 +0000

    Restart the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+import './App.css';
+
+function App() {
+  return (
+    <PrimaryButton>Hello, World!</PrimaryButton>
+  );
+}
+
+export default App;
